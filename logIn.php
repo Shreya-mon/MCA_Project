b9474e9ca4f284bb919733c2b1b7174abc88369f
unknown <monshreya123@gmail.com>
2022-11-22 00:19:57 +0530

    Jayanta

--- logIn.php
+++ logIn.php
@@ -1,19 +1,211 @@
+<!DOCTYPE html>
 <html>
-    <head><!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-  rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
-  rel="stylesheet"
-/></head>
+<head>
+    <meta charset="utf-8">
+        
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/brands.min.css"
+        integrity="sha512-apX8rFN/KxJW8rniQbkvzrshQ3KvyEH+4szT3Sno5svdr6E/CP0QE862yEeLBMUnCqLko8QaugGkzvWS7uNfFQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--<link rel="stylesheet" href="frontend/css/styles.css?V="-->
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- <style>
+       
+        html,body {
+            height: 100%;
+            background-color: #152733;
+        }
+        .button {
+
+            background-color: black;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+        ::placeholder {
+            color: white;
+            text-align: center;
+        }
+
+
+        .form-content {
+            border: 0;
+            background-color: #ebeff8;
+            color: #8D8D8D;
+
+        }
+
+        .fa {
+            color: black;
+        }
+
+        .fas {
+            color: black;
+        }
+
+        .eye {
+            position: absolute;
+            margin-top: -8vmin;
+            margin-left: 37vmin;
+
+        }
+
+        #hide1 {
+            display: none;
+
+        }   
+    </style>  -->
+    <style>
+        body {
+  font-family: "Karla", sans-serif;
+  background-color: #d0d0ce;
+  min-height: 100vh; }
+
+.brand-wrapper {
+  margin-bottom: 19px; }
+  .brand-wrapper .logo {
+    height: 37px; }
+
+.login-card {
+  border: 0;
+  border-radius: 27.5px;
+  box-shadow: 0 10px 30px 0 rgba(172, 168, 168, 0.43);
+  overflow: hidden; }
+  .login-card-img {
+    border-radius: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+       object-fit: cover; }
+  .login-card .card-body {
+    padding: 85px 60px 60px; }
+    .eye {
+            position: absolute;
+            margin-top: -2vmin;
+            margin-left: 45vmin;
+
+        }
+        #hide1 {
+            display: none;
+
+        }   
+
+    @media (max-width: 1024px) {
+      .eye {
+            margin-top: -9vmin;
+            margin-left: 50vmin;
+        }
+    }
+    @media (max-width: 767px) {
+      .login-card-footer-text {
+        margin-bottom: 24px; } }
+  .login-card-footer-nav a {
+    font-size: 14px;
+    color: #919aa3; }
+
+.footer-link {
+  position: absolute;
+  bottom: 1rem;
+  text-align: center;
+  width: 100%; }
+  .eye {
+            margin-top: -9vmin;
+            margin-left: 45vmin;
+        }
+    @media (max-width: 422px) {
+      .login-card .card-body {
+        padding: 35px 24px; }
+        .eye {
+            margin-top: -12vmin;
+            margin-left: 70vmin;
+        }
+     }
+     @media (max-width: 375px) {
+      .eye {
+            margin-top: -14vmin;
+        }
+     }
+     @media (max-width: 320px) {
+      .eye {
+            margin-top: -16vmin;
+            margin-left: 65vmin;
+        }
+     }
+  .login-card-description {
+    font-size: 25px;
+    color: #000;
+    font-weight: normal;
+    margin-bottom: 23px; }
+  .login-card form {
+    max-width: 326px; }
+  .login-card .form-control {
+    border: 1px solid #d5dae2;
+    padding: 15px 25px;
+    margin-bottom: 20px;
+    min-height: 45px;
+    font-size: 13px;
+    line-height: 15;
+    font-weight: normal; }
+    .login-card .form-control::-webkit-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::-moz-placeholder {
+      color: #919aa3; }
+    .login-card .form-control:-ms-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::-ms-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::placeholder {
+      color: #919aa3; }
+  .login-card .login-btn {
+    padding: 13px 20px 12px;
+    background-color: #000;
+    border-radius: 4px;
+    font-size: 17px;
+    font-weight: bold;
+    line-height: 20px;
+    color: #fff;
+    margin-bottom: 24px; }
+    .login-card .login-btn:hover {
+      border: 1px solid #000;
+      background-color: transparent;
+      color: #000; }
+  .login-card .forgot-password-link {
+    font-size: 14px;
+    color: #919aa3;
+    margin-bottom: 12px; }
+  .login-card-footer-text {
+    font-size: 16px;
+    color: #0d2366;
+    margin-bottom: 60px; }
+    </style>
+</head>
+<body>
 <section class="vh-100" style="background-color: #9A616D;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
@@ -65,8 +257,13 @@
     </div>
   </div>
 </section>
-</html>
 
+
+
+
+</body>
+
+</html>
 
 <?php
 include("connection.php");
@@ -87,19 +284,17 @@ if($result=mysqli_query($link, $qry))
 			if($rowc1 == 0)
 			{   
 				$_SESSION['errorPass'] = "Incorrect Password!";
+        
 				header('Location:logIn.php');
                 
 			}
 			else
 			{
 				$row = mysqli_fetch_assoc($result);
-				
-				
 				$_SESSION['name'] = $row['name'];
 				$_SESSION['email'] = $row['email'];
-				
-				
-                header('Location:home.html');
+        echo "<script>window.location.href='home.html';</script>";
+				 header('Location:home.html');
 			}
 		}
 	}
@@ -112,6 +307,5 @@ if($result=mysqli_query($link, $qry))
 }
 }
 ?>
-
 
 
--- signup.php
+++ signup.php
@@ -1,21 +1,215 @@
 
 
 <html>
-    <head><!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-  rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
-  rel="stylesheet"
-/></head>
+
+<head>
+    <meta charset="utf-8">
+        
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/brands.min.css"
+        integrity="sha512-apX8rFN/KxJW8rniQbkvzrshQ3KvyEH+4szT3Sno5svdr6E/CP0QE862yEeLBMUnCqLko8QaugGkzvWS7uNfFQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--<link rel="stylesheet" href="frontend/css/styles.css?V="-->
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- <style>
+       
+        html,body {
+            height: 100%;
+            background-color: #152733;
+        }
+        .button {
+
+            background-color: black;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+        ::placeholder {
+            color: white;
+            text-align: center;
+        }
+
+
+        .form-content {
+            border: 0;
+            background-color: #ebeff8;
+            color: #8D8D8D;
+
+        }
+
+        .fa {
+            color: black;
+        }
+
+        .fas {
+            color: black;
+        }
+
+        .eye {
+            position: absolute;
+            margin-top: -8vmin;
+            margin-left: 37vmin;
+
+        }
+
+        #hide1 {
+            display: none;
+
+        }   
+    </style>  -->
+    <style>
+        body {
+  font-family: "Karla", sans-serif;
+  background-color: #d0d0ce;
+  min-height: 100vh; }
+
+.brand-wrapper {
+  margin-bottom: 19px; }
+  .brand-wrapper .logo {
+    height: 37px; }
+
+.login-card {
+  border: 0;
+  border-radius: 27.5px;
+  box-shadow: 0 10px 30px 0 rgba(172, 168, 168, 0.43);
+  overflow: hidden; }
+  .login-card-img {
+    border-radius: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+       object-fit: cover; }
+  .login-card .card-body {
+    padding: 85px 60px 60px; }
+    .eye {
+            position: absolute;
+            margin-top: -2vmin;
+            margin-left: 45vmin;
+
+        }
+        #hide1 {
+            display: none;
+
+        }   
+
+    @media (max-width: 1024px) {
+      .eye {
+            margin-top: -9vmin;
+            margin-left: 50vmin;
+        }
+    }
+    @media (max-width: 767px) {
+      .login-card-footer-text {
+        margin-bottom: 24px; } }
+  .login-card-footer-nav a {
+    font-size: 14px;
+    color: #919aa3; }
+
+.footer-link {
+  position: absolute;
+  bottom: 1rem;
+  text-align: center;
+  width: 100%; }
+  .eye {
+            margin-top: -9vmin;
+            margin-left: 45vmin;
+        }
+    @media (max-width: 422px) {
+      .login-card .card-body {
+        padding: 35px 24px; }
+        .eye {
+            margin-top: -12vmin;
+            margin-left: 70vmin;
+        }
+     }
+     @media (max-width: 375px) {
+      .eye {
+            margin-top: -14vmin;
+        }
+     }
+     @media (max-width: 320px) {
+      .eye {
+            margin-top: -16vmin;
+            margin-left: 65vmin;
+        }
+     }
+  .login-card-description {
+    font-size: 25px;
+    color: #000;
+    font-weight: normal;
+    margin-bottom: 23px; }
+  .login-card form {
+    max-width: 326px; }
+  .login-card .form-control {
+    border: 1px solid #d5dae2;
+    padding: 15px 25px;
+    margin-bottom: 20px;
+    min-height: 45px;
+    font-size: 13px;
+    line-height: 15;
+    font-weight: normal; }
+    .login-card .form-control::-webkit-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::-moz-placeholder {
+      color: #919aa3; }
+    .login-card .form-control:-ms-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::-ms-input-placeholder {
+      color: #919aa3; }
+    .login-card .form-control::placeholder {
+      color: #919aa3; }
+  .login-card .login-btn {
+    padding: 13px 20px 12px;
+    background-color: #000;
+    border-radius: 4px;
+    font-size: 17px;
+    font-weight: bold;
+    line-height: 20px;
+    color: #fff;
+    margin-bottom: 24px; }
+    .login-card .login-btn:hover {
+      border: 1px solid #000;
+      background-color: transparent;
+      color: #000; }
+  .login-card .forgot-password-link {
+    font-size: 14px;
+    color: #919aa3;
+    margin-bottom: 12px; }
+  .login-card-footer-text {
+    font-size: 16px;
+    color: #0d2366;
+    margin-bottom: 60px; }
+    </style>
+</head>
+
+<body>
+
 <section class="vh-100" style="background-color: #9A616D;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
@@ -37,7 +231,6 @@
                   </div>
 
                   <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-                  
                   <div class="form-outline mb-4">
                     <input type="text" name="name" id="form2Example17" class="form-control form-control-lg" />
                     <label class="form-label" for="form2Example17">Name</label>
@@ -45,21 +238,21 @@
 
                   <div class="form-outline mb-4">
                     <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" />
-                    <label class="form-label" for="form2Example17">Email</label>
+                    <label class="form-label" for="form2Example17">Email address</label>
                   </div>
 
                   <div class="form-outline mb-4">
-                    <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" />
+                    <input type="password"  name="password" id="form2Example27" class="form-control form-control-lg" />
                     <label class="form-label" for="form2Example27">Password</label>
                   </div>
 
                   <div class="pt-1 mb-4">
-                    <input type="submit" name="submit" class="btn btn-dark btn-lg btn-block" value="Register">
+                    <input type="submit" name="submit" value="REGISTER" class="btn btn-dark btn-lg btn-block">
                   </div>
 
                   <a class="small text-muted" href="#!">Forgot password?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Already have an account? <a href="logIn.php"
-                      style="color: #393f81;">Log In here</a></p>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Already Have an account? <a href="logIn.php"
+                      style="color: #393f81;">LOG In here</a></p>
                   <a href="#!" class="small text-muted">Terms of use.</a>
                   <a href="#!" class="small text-muted">Privacy policy</a>
                 </form>
@@ -72,22 +265,25 @@
     </div>
   </div>
 </section>
+
+
+
+
+</body>
+
 </html>
 
 
 
 <?php
 include("connection.php");
-?>
 
-<?php
 if(isset($_POST['submit'])){
     extract ($_POST);
     $sql="SELECT * FROM `user_details` WHERE `email`='$email'";
     $res = mysqli_query($link, $sql);
     if(mysqli_num_rows($res)>0){
-
-               
+          
             
            $error[]='Email Already Exists.'; 
     }
@@ -98,9 +294,8 @@ if(isset($_POST['submit'])){
 								 '".$_REQUEST['password']."','')";
 	$result=mysqli_query($link, $sql);
     if ($result){
+      echo "<script>window.location.href='home.html';</script>";
         header("Location: http:\MCA_Project\home.html");
-       
-
    }
     else{?>
         <script>
